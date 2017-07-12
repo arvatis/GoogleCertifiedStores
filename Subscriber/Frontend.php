7efbdb122794a99dedc087b6ad7c501ef91c5eae
phY <pwnyprod@users.noreply.github.com>
2017-07-12 13:37:07 +0200

    Updated to new Pluginstruct & Fixed Shopscope (#3)
    * Updated to new Pluginstruct & Fixed Shopscope
    
    * after merge
    
    * optimized config read
    
    * optimized config read & cs fixer
    
    * optimized config read & cs fixer
    
    * removed comment
    
    * changelog

--- Subscriber/Frontend.php
+++ Subscriber/Frontend.php
@@ -3,22 +3,48 @@
 namespace ArvGoogleCertifiedShops\Subscriber;
 
 use Enlight\Event\SubscriberInterface;
-use \Exception;
-use \Zend_Date;
+use Exception;
+use Shopware\Components\Plugin\CachedConfigReader;
+use Shopware\Components\Plugin\ConfigReader;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Zend_Date;
 
 class Frontend implements SubscriberInterface
 {
+    /**
+     * @var CachedConfigReader|ConfigReader
+     */
+    private $configReader;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Frontend constructor.
+     *
+     * @param CachedConfigReader|ConfigReader $configReader
+     * @param ContainerInterface              $container
+     */
+    public function __construct(ConfigReader $configReader, ContainerInterface $container)
+    {
+        $this->configReader = $configReader;
+        $this->container = $container;
+    }
+
     public static function getSubscribedEvents()
     {
-        return array(
-            'Enlight_Controller_Action_PostDispatch_Frontend' => 'onPostDispatch'
-        );
+        return [
+            'Enlight_Controller_Action_PostDispatch_Frontend' => 'onPostDispatch',
+        ];
     }
 
     /**
      * Event listener method
      *
      * @param \Enlight_Controller_ActionEventArgs $args
+     *
      * @throws \Zend_Date_Exception
      * @throws \Exception
      */
@@ -31,10 +57,7 @@ class Frontend implements SubscriberInterface
             return;
         }
 
-        $config = Shopware()->Container()->get('shopware.plugin.config_reader')->getByPluginName('ArvGoogleCertifiedShops');
-
-        $value = $config['TRUSTED_STORE_ID'];
-        if (empty($value)) {
+        if ($this->getConfigVar('TRUSTED_STORE_ID')) {
             return;
         }
 
@@ -54,23 +77,39 @@ class Frontend implements SubscriberInterface
 
         try {
             $now = new Zend_Date();
-            $dateShipping = $now->addDay($config['ORDER_EST_SHIP_DATE'])->toString('YYYY-MM-dd');
+            $dateShipping = $now->addDay($this->getConfigVar('ORDER_EST_SHIP_DATE'))->toString('YYYY-MM-dd');
 
             $now = new Zend_Date();
-            $dateDelivery = $now->addDay($config['ORDER_EST_DELIVERY_DATE'])->toString('YYYY-MM-dd');
+            $dateDelivery = $now->addDay($this->getConfigVar('ORDER_EST_DELIVERY_DATE'))->toString('YYYY-MM-dd');
         } catch (Exception $e) {
             $dateShipping = $dateDelivery = new Zend_Date();
         }
-        $view->assign('ARV_GTS_TRUSTED_STORE_ID', $config['TRUSTED_STORE_ID']);
-        $view->assign('ARV_GTS_BADGE_POSITION', $config['BADGE_POSITION']);
+        $view->assign('ARV_GTS_TRUSTED_STORE_ID', $this->getConfigVar('TRUSTED_STORE_ID'));
+        $view->assign('ARV_GTS_BADGE_POSITION', $this->getConfigVar('BADGE_POSITION'));
         $view->assign('ARV_GTS_LOCALE', Shopware()->Locale()->toString());
         $view->assign('ARV_GTS_COUNTRY', Shopware()->Locale()->getRegion());
-        $view->assign('ARV_GTS_MERCHANT_ORDER_DOMAIN', $config['MERCHANT_ORDER_DOMAIN']);
+        $view->assign('ARV_GTS_MERCHANT_ORDER_DOMAIN', $this->getConfigVar('MERCHANT_ORDER_DOMAIN'));
         $view->assign('ARV_GTS_ORDER_EST_SHIP_DATE', $dateShipping);
         $view->assign('ARV_GTS_ORDER_EST_DELIVERY_DATE', $dateDelivery);
         $view->assign('ARV_GTS_BASKET_CURRENCY', Shopware()->Currency()->getShortName());
-        $view->assign('ARV_GTS_GOOGLE_SHOPPING_ACCOUNT_ID', $config['GOOGLE_SHOPPING_ACCOUNT_ID']);
-        $view->assign('ARV_GTS_GOOGLE_SHOPPING_COUNTRY', $config['GOOGLE_SHOPPING_COUNTRY']);
-        $view->assign('ARV_GTS_GOOGLE_GOOGLE_SHOPPING_LANGUAGE', $config['GOOGLE_SHOPPING_LANGUAGE']);
+        $view->assign('ARV_GTS_GOOGLE_SHOPPING_ACCOUNT_ID', $this->getConfigVar('GOOGLE_SHOPPING_ACCOUNT_ID'));
+        $view->assign('ARV_GTS_GOOGLE_SHOPPING_COUNTRY', $this->getConfigVar('GOOGLE_SHOPPING_COUNTRY'));
+        $view->assign('ARV_GTS_GOOGLE_GOOGLE_SHOPPING_LANGUAGE', $this->getConfigVar('GOOGLE_SHOPPING_LANGUAGE'));
+    }
+
+    /**
+     * @param string      $var
+     * @param bool|string $default
+     *
+     * @return bool|string
+     */
+    private function getConfigVar($var, $default = false)
+    {
+        $config = $this->configReader->getByPluginName('ArvGoogleRemarketing', $this->container->get('shop'));
+        if (empty($config[$var])) {
+            return $default;
+        }
+
+        return $config[$var];
     }
 }
